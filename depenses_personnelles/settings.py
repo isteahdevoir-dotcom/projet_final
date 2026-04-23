676784540f9fbeea59275a080d1f0a60d3306140
Annaele Pierre <isteahdevoir@gmail.com>
2026-04-23 18:34:24 -0400

    ajout static

--- depenses_personnelles/settings.py
+++ depenses_personnelles/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yd0wrcglboly*#^_p)!q8w-i$p$r=(b-r7#+*__@3wm5iq5fmw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['annaele.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
